8aff2e563754eafcb2c0176b2f3beea9109eb44d
Simon <simon@zeitlinger.de>
2021-01-01 14:10:35 +0100

    finale Version

--- 4 gewinnt.kt
+++ 4 gewinnt.kt
@@ -2,7 +2,6 @@
 
 package gew
 
-import kotlin.math.pow
 import kotlin.streams.toList
 
 var brett = mutableListOf(
@@ -14,10 +13,10 @@ var brett = mutableListOf(
         0, 0, 0, 0, 0, 0, 0
 )
 
-enum class Schwierigkeit(var minMax: Int, val zeit: Int) {
-    leicht(minMax = 1, zeit = 1),
-    mittel(minMax = 1, zeit = 5),
-    schwer(minMax = 1, zeit = 10)
+enum class Schwierigkeit(var minMax: Int, var monteCarlo: Int) {
+    leicht(minMax = 1, monteCarlo = 25000),
+    mittel(minMax = 1, monteCarlo = 125000),
+    schwer(minMax = 1, monteCarlo = 250000)
 }
 
 
@@ -25,20 +24,45 @@ fun menschZug(züge: Int) {
     println("Du bist am Zug ${
         if (züge < 2) {
             "(1 - 7)"
-        } else ""
+        }
+    else ""
     } ")
+    if (züge < 2) {
+        println("  1 2 3 4 5 6 7  ")
+        println("+---------------+")
+        var zeile = 0
+        while (zeile < 6) {
+            var spalte = 0
+            print("| ")
+            while (spalte < 7) {
+                print(when (brett[zeile * 7 + spalte]) {
+                    0 -> "  "
+                    1 -> "o "
+                    else -> "x "
+                })
 
-    while (true) {
-        val eingabe = readLine()?.toIntOrNull()
-        if (eingabe == null || eingabe < 1 || eingabe > 7 || brett[eingabe - 1] != 0) {
-            println("Falsche Eingabe")
-            continue
+                spalte += 1
+
+            }
+
+            println("|")
+            zeile += 1
         }
-        if (zug(eingabe - 1, brett, -1)) {
-            return
-        }
-        println("Falsche Eingabe")
+        println("+---------------+")
     }
+
+
+while (true) {
+    val eingabe = readLine()?.toIntOrNull()
+    if (eingabe == null || eingabe < 1 || eingabe > 7 || brett[eingabe - 1] != 0) {
+        println("Falsche Eingabe")
+        continue
+    }
+    if (zug(eingabe - 1, brett, -1)) {
+        return
+    }
+    println("Falsche Eingabe")
+}
 }
 
 data class Knoten(val kinder: List<Knoten>, var wert: Int = 0, val brett: List<Int>, val zug: Int, val dran: Int)
@@ -64,40 +88,20 @@ fun generieren(brett: List<Int>, feld: Int, dran: Int, züge: Int, tabelle: Muta
 
     val knoten = Knoten(kinder, brett = brett, zug = feld, dran = dran)
     tabelle[brett] = knoten
-
-    if (gewonnen(knoten.brett, -1)) {
-        knoten.wert = Int.MIN_VALUE
-    } else if (gewonnen(knoten.brett, 1)) {
-        knoten.wert = Int.MAX_VALUE
-    }
-
-    if (kinder.isNotEmpty()) {
-        val w = if (knoten.dran == 1) {
-            knoten.kinder.maxOf { kind -> kind.wert }
-        } else {
-            knoten.kinder.minOf { kind -> kind.wert }
-        }
-        if (w == Int.MAX_VALUE || w == Int.MIN_VALUE) {
-            knoten.wert = w
-        }
-    }
-
     return knoten
 }
 
-fun monte2(knoten: Knoten) {
-    if (knoten.wert != 0) {
+fun minMax(minMax: Int, knoten: Knoten, monteCarlo: Int) {
+    if (gewonnen(knoten.brett, -1)) {
+        knoten.wert = -monteCarlo - 1
         return
+    } else if (gewonnen(knoten.brett, 1)) {
+        knoten.wert = monteCarlo + 1
+        return
+    } else if (knoten.kinder.isEmpty()) {
+        knoten.wert = monteCarlo(brett = knoten.brett, dran = knoten.dran * -1, knoten = knoten, monteCarlo)
     }
-
-    monteCarlo(brett = knoten.brett, dran = knoten.dran, knoten, 1)
-    knoten.kinder.forEach {
-        monte2(it)
-    }
-}
-
-fun minMax(minMax: Int, knoten: Knoten) {
-    knoten.kinder.forEach { kind -> minMax(minMax * -1, kind) }
+    knoten.kinder.forEach { kind -> minMax(minMax * -1, kind, monteCarlo) }
 
     if (knoten.kinder.isEmpty()) {
         return
@@ -114,6 +118,7 @@ fun monteCarlo(brett: List<Int>, dran: Int, knoten: Knoten, monteCarlo: Int): In
     var durchläufe = monteCarlo
     var wert = 0
     val züge = 0
+    val zeit = System.currentTimeMillis()
     wert = (0..durchläufe).toList().parallelStream()
             .map { durchlauf(brett = brett.toMutableList(), dran = dran, knoten = knoten, wert = wert, züge = züge) }
             .toList().sum()
@@ -176,18 +181,13 @@ fun computerZug(s: Schwierigkeit, zug: Int) {
     if (s == Schwierigkeit.schwer) {
         println("Bitte warten...")
     }
+    if (zug < 3) {
+        zug(3, brett, 1)
+        return
+    }
     val zeit = System.currentTimeMillis()
     var k = generieren(brett.toMutableList(), -1, 1, züge = s.minMax, mutableMapOf())
-
-var durchläufe = 0
-    while (System.currentTimeMillis() - zeit < s.zeit * 1000) {
-        monte2(k)
-        durchläufe += 1
-    }
-    println("durchläufe: "+durchläufe)
-
-
-    minMax(1, k)
+    minMax(1, k, s.monteCarlo)
     val max = k.kinder.maxOf { kind -> kind.wert }
     val liste = mutableListOf<Knoten>()
     k.kinder.forEach { kind ->
@@ -197,6 +197,7 @@ var durchläufe = 0
     }
     val zug = liste.random().zug
     zug(zug, brett, 1)
+
     println("zeit: " + (System.currentTimeMillis() - zeit) / 1000)
 }
 
@@ -276,6 +277,25 @@ fun start(): Int {
 
 fun main() {
     System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "8")
+
+    while (true) {
+        partie()
+
+        println("Wills du erneut spielen? (j/n)")
+        while (true) {
+            val e = readLine()
+            if (e == "j") {
+                brett.forEachIndexed{index, i -> brett[index] = 0 }
+                break
+            } else if (e == "n") {
+                return
+            }
+            println("Falsche Eingabe")
+        }
+    }
+}
+
+private fun partie() {
     val s = schwierigkeit()
     var dran = start()
     var runde = 1
@@ -283,40 +303,52 @@ fun main() {
         if (runde == 10) {
             if (s == Schwierigkeit.schwer) {
                 s.minMax = 2
+                s.monteCarlo = 50000
             }
         } else if (runde == 15) {
             if (s == Schwierigkeit.schwer) {
                 s.minMax = 3
+                s.monteCarlo = 10000
             } else if (s == Schwierigkeit.mittel) {
                 s.minMax = 2
+                s.monteCarlo = 25000
             }
         } else if (runde == 20) {
             if (s == Schwierigkeit.schwer) {
                 s.minMax = 4
+                s.monteCarlo = 5000
             } else if (s == Schwierigkeit.mittel) {
                 s.minMax = 3
+                s.monteCarlo = 5000
             }
             if (s == Schwierigkeit.leicht) {
                 s.minMax = 2
+                s.monteCarlo = 5000
             } else if (runde == 25) {
                 if (s == Schwierigkeit.schwer) {
                     s.minMax = 5
+                    s.monteCarlo = 4000
                 } else if (s == Schwierigkeit.mittel) {
                     s.minMax = 4
+                    s.monteCarlo = 2000
                     if (s == Schwierigkeit.leicht) {
                         s.minMax = 3
+                        s.monteCarlo = 1000
                     }
-                } else if (runde == 34) {
+                } else if (runde == 26) {
                     if (s == Schwierigkeit.schwer) {
-                        s.minMax = 8
+                        s.minMax = 16
+                        s.monteCarlo = 1
                     }
-                } else if (runde == 36) {
+                } else if (runde == 27) {
                     if (s == Schwierigkeit.mittel) {
-                        s.minMax = 6
+                        s.minMax = 15
+                        s.monteCarlo = 1
                     }
-                } else if (runde == 38) {
+                } else if (runde == 31) {
                     if (s == Schwierigkeit.leicht) {
-                        s.minMax = 4
+                        s.minMax = 11
+                        s.monteCarlo = 1
                     }
                 }
             }
